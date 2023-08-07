5aec4712bad371e1f3fe1339d7b51f99674b92f4
RobelHailu <116779961+Robel-003@users.noreply.github.com>
2023-08-07 14:58:03 -0700

    product functionality added (#28)

--- app/src/main/java/com/example/nsc_events/Routes.kt
+++ app/src/main/java/com/example/nsc_events/Routes.kt
@@ -3,4 +3,6 @@ package com.example.nsc_events
 sealed class Routes(val route: String) {
     object Login : Routes("Login")
     object SignUp : Routes("SignUp")
+
+    object AddEvent: Routes("Add Event")
 }
