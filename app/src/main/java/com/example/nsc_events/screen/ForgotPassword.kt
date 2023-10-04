8f11bde8e4ab247bd838111d633d76936721cfaf
taylorpapke <taylorpapke@gmail.com>
2023-10-04 00:38:51 -0700

    created forgot password hyperlink to page

--- app/src/main/java/com/example/nsc_events/screen/ForgotPassword.kt
+++ app/src/main/java/com/example/nsc_events/screen/ForgotPassword.kt
@@ -1,6 +1,5 @@
 package com.example.nsc_events.screen
 
-import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -33,13 +31,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -47,26 +43,21 @@ import com.example.nsc_events.R
 import com.example.nsc_events.Routes
 
 @Composable
-fun LoginPage(navController: NavHostController) {
+fun ForgotPasswordPage(navController: NavHostController) {
     var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
     var isEmailValid by remember { mutableStateOf(true) }
-    var isPasswordValid by remember { mutableStateOf(true) }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         IconButton(
-            onClick = { navController.navigate(Routes.HomePage.route) },
+            onClick = { navController.navigate(Routes.Login.route) },
             modifier = Modifier.align(Alignment.Start)
         ) {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Home")
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Login")
         }
-
         Image(
             painter = painterResource(id = R.drawable.packaging),
             contentDescription = stringResource(id = R.string.login_logo_description),
@@ -117,28 +108,6 @@ fun LoginPage(navController: NavHostController) {
                     ErrorDisplay(text = stringResource(id = R.string.login_email_error))
                 }
 
-                // Password field
-                TextField(
-                    value = password,
-                    onValueChange = {
-                        password = it
-                        isPasswordValid = validatePassword(it)
-                    },
-                    isError = !isPasswordValid,
-                    label = { Text(text = stringResource(id = R.string.login_password)) },
-                    singleLine = true,
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 30.dp, end = 30.dp, bottom = 20.dp),
-                )
-
-                // Password display error
-                if (!isPasswordValid) {
-                    ErrorDisplay(text = stringResource(id = R.string.login_password_error))
-                }
-
                 // Login button
                 Button(
                     onClick = {
@@ -162,85 +131,10 @@ fun LoginPage(navController: NavHostController) {
                         )
                     )
                 }
-
-                // Forgot password
-                ClickableText(
-                    text = AnnotatedString("Forgot password?"),
-                    onClick = {
-                         navController.navigate(Routes.ForgotPassword.route)
-                        // TODO: Add forgot password functionality
-                    },
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        fontFamily = FontFamily.Default,
-                        textDecoration = TextDecoration.Underline,
-                        color = if (isSystemInDarkTheme()) Color.LightGray else Color.Blue
-                    ),
-                    modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                        .align(Alignment.CenterHorizontally)
-                )
-
-
-                /* Add event button */
-                Button(
-                    onClick = {
-                        navController.navigate(Routes.AddEvent.route)
-                    },
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(200.dp)
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    Text(text = "Add Event")
-                }
             }
         }
-
-    }
-
-    // Create a new account
-    Box(modifier = Modifier.fillMaxSize()) {
-        ClickableText(
-            text = AnnotatedString(text = stringResource(id = R.string.login_create_account)),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(20.dp),
-            onClick = { navController.navigate(Routes.SignUp.route) },
-            style = TextStyle(
-                fontSize = 14.sp,
-                fontFamily = FontFamily.Default,
-                textDecoration = TextDecoration.Underline,
-                color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black
-            )
-        )
     }
 }
 
-// Validation functions
-fun validateEmail(email: String): Boolean {
-    return Patterns.EMAIL_ADDRESS.matcher(email)
-        .matches() && email.isNotEmpty() && email.isNotBlank() && email.length > 5 && email.length < 30 && email.contains(
-        "@"
-    )
-}
 
-fun validatePassword(password: String): Boolean {
-    return password.isNotEmpty() && password.isNotBlank() && password.length > 5 && password.length < 30
-}
 
-// Error display function
-@Composable
-fun ErrorDisplay(text: String) {
-    Text(
-        text = text,
-        color = Color.Red,
-        style = TextStyle(
-            fontSize = 14.sp,
-            fontFamily = FontFamily.Default,
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 30.dp, end = 30.dp, bottom = 8.dp)
-    )
-}
