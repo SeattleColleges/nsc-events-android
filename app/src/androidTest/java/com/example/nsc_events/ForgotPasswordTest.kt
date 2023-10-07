78e649d61db0e087a24d6f3f66c8c1c29cf005fa
taylorpapke <taylorpapke@gmail.com>
2023-10-07 14:17:58 -0700

    created 2nd test

--- app/src/androidTest/java/com/example/nsc_events/ForgotPasswordTest.kt
+++ app/src/androidTest/java/com/example/nsc_events/ForgotPasswordTest.kt
@@ -1,21 +1,15 @@
 package com.example.nsc_events
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.nsc_events.screen.ForgotPasswordPage
-import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,7 +21,7 @@ class ForgotPasswordTest {
     val rule =
         createComposeRule()   // compose rule is required to get access to the composable component
 
-    lateinit var navController: TestNavHostController
+    private lateinit var navController: TestNavHostController
 
     @Before
     fun setUp() {
@@ -39,17 +33,18 @@ class ForgotPasswordTest {
     }
 
     @Test
-    fun verify_StartDestinationIsForgotPasswordScreen() {
-        val idToString = R.string.forgot_password_button_text.toString()
+    fun verifyStartDestinationIsForgotPasswordScreen() {
         rule
             .onNodeWithText("Forgot Password?")
             .assertIsDisplayed()
     }
 
     @Test
-    fun emailNotInHierarchy() {
-        // Check that the email address is not in the hierarchy.
-        Espresso.onView(withText(R.string.forgot_password_email)).check(doesNotExist())
+    fun verifyTextEnteredIntoEmail() {
+        rule
+            .onNodeWithText("Email").performTextInput("1")
+        rule
+            .onNodeWithText("Email").assertTextContains("1")
     }
 }
 
