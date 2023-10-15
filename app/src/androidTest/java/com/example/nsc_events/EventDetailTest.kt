e968af46a3ca77148da761b8124ecbbee0ceac25
taylorpapke <taylorpapke@gmail.com>
2023-10-15 01:39:52 -0700

    added a couple tests to EventDetail and HomePage

---
+++ app/src/androidTest/java/com/example/nsc_events/EventDetailTest.kt
@@ -0,0 +1,48 @@
+package com.example.nsc_events
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.nsc_events.screen.EventDetailPage
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)  // To indicate that we've to run it with AndroidJUnit runner
+class EventDetailTest {
+    @get: Rule
+    val rule =
+        createComposeRule()   // compose rule is required to get access to the composable component
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setUp() {
+        rule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            EventDetailPage(navController = navController, "Thanksgiving Pageant")
+        }
+    }
+
+    @Test
+    fun checkNumberOfMatchedNodesOnEventDetailPage() {
+        rule
+            .onAllNodesWithText("Thanksgiving Pageant")
+            .assertCountEquals(1)
+    }
+
+    @Test
+    fun assertAddEventIsClickableOnEventDetailPage() {
+        rule
+            .onNodeWithContentDescription("Back to Home page")
+            .assertHasClickAction()
+    }
+}
