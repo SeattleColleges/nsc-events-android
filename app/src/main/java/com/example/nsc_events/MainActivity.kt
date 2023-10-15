190aee678d2a3ab6ff589946a64d5852d8031d7d
Group 6 <130420964+kyle-nsc-ad470-group6@users.noreply.github.com>
2023-10-15 16:22:09 -0700

    Create scaffold for navigation links

--- app/src/main/java/com/example/nsc_events/MainActivity.kt
+++ app/src/main/java/com/example/nsc_events/MainActivity.kt
@@ -1,15 +1,31 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.nsc_events
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.nsc_events.screen.ScreenMain
 import com.example.nsc_events.ui.theme.Nsc_eventsTheme
 
@@ -23,7 +39,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScreenMain()
+                    //ScreenMain()
+                    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        topBar = {
+                            CenterAlignedTopAppBar(
+                                title = {
+                                    Text(text = "My NSC Events")
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = { /*TODO pop the nav stack*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Menu,
+                                            contentDescription = "Go back"
+                                        )
+                                     }
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.FavoriteBorder,
+                                            contententDescription = "Mark as favorite"
+                                        )
+                                    }
+
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Edit,
+                                            contententDescription = "Edit my events"
+                                        )
+                                    }
+                                },
+                                scrollBehavior = scrollBehavior
+                                )
+                        }
+                    ) { values ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(values)
+                        ) {
+                            items(100) {
+                                Text(
+                                    text = "Item$it",
+                                    modifier = Modifier.padding(16.dp)
+                                    )
+                            }
+                        }
+
+
+                    }
                 }
             }
         }
