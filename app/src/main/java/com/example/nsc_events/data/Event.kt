d1e0e428a4126c0892356d9acd0d5c0b8e67765e
TroyFPV48 <77989401+TroyFPV48@users.noreply.github.com>
2023-10-20 15:23:07 -0700

    actvity formatted

--- app/src/main/java/com/example/nsc_events/data/Event.kt
+++ app/src/main/java/com/example/nsc_events/data/Event.kt
@@ -2,10 +2,38 @@ package com.example.nsc_events.data
 
 import java.util.Date
 
+data class SocialMedia(
+    val facebook: String,
+    val twitter: String,
+    val instagram: String,
+    val hashtag: String
+)
+
 data class Event(
-    val eventName: String,
-    val description: String,
+    val eventTitle: String,
+    val eventDescription: String,
     /* TODO: change date variable to a Date data type instead of string */
-    val date: String
+    val eventCategory: String,
+    val date: Date,
+    val eventStartTime: Date,
+    val eventEndTime: Date,
+    val eventLocation: String,
+    val eventCoverPhoto: String,
+    val eventHost: String,
+    val eventWebsite: String,
+    val eventRegistration: String,
+    val eventCapacity: String,
+    val eventCost: String,
+    val eventTags: List<String>,
+    val eventSchedule: String,
+    val eventSpeakers: List<String>,
+    val eventPrerequisites: String,
+    val eventCancellationPolicy: String,
+    val eventContact: String,
+    val eventSocialMedia: SocialMedia,
+    val eventPrivacy: String,
+    val eventAccessibility: String,
+    val eventVisibility: Boolean
+
     /* TODO: what other event properties should be added? */
 )
