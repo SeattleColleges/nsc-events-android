e015992164ce52394227ce67ba760bfc044893c7
TroyFPV48 <77989401+TroyFPV48@users.noreply.github.com>
2023-10-25 10:44:35 -0700

    delete functionality

--- app/src/main/java/com/example/nsc_events/data/Event.kt
+++ app/src/main/java/com/example/nsc_events/data/Event.kt
@@ -6,6 +6,13 @@ data class Event(
     val eventName: String,
     val description: String,
     /* TODO: change date variable to a Date data type instead of string */
-    val date: String
+    val date: String,
+    var eventVisibility : Boolean
     /* TODO: what other event properties should be added? */
-)
+) {
+
+    fun deleteEvent() {
+        this.eventVisibility = false
+
+    }
+}
