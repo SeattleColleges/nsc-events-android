1f800db255362013e67aaa2786394a3e68fdb160
Ali Sharif <alisharif29@gmail.com>
2023-10-31 10:28:36 -0700

    Add Name field & Password match toast

--- app/src/main/java/com/example/nsc_events/screen/SignUp.kt
+++ app/src/main/java/com/example/nsc_events/screen/SignUp.kt
@@ -1,5 +1,7 @@
 package com.example.nsc_events.screen
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,12 +22,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.nsc_events.MainActivity
+import com.example.nsc_events.Routes
+import kotlin.math.min
 
 @Composable
 fun SignUpPage( navController: NavHostController) {
@@ -32,7 +41,11 @@ fun SignUpPage( navController: NavHostController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
+    fun displayToast(context: Context) {
+        Toast.makeText(context, "Passwords do not match", Toast.LENGTH_SHORT).show()
+    }
 
     Column (
         modifier = Modifier
@@ -58,13 +71,24 @@ fun SignUpPage( navController: NavHostController) {
                     )
             ) {
                 // TODO: Name Field here
-
+                TextField(value = name, onValueChange ={
+                    name = it
+                },
+                    label = { Text(text = "Name")},
+                    maxLines = 1,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
+                    )
 
                 // Email field
                 TextField(value = email, onValueChange ={
                     email = it
                 },
                     label = { Text(text = "Email")},
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Email
+                    ),
                     maxLines = 1,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -76,6 +100,10 @@ fun SignUpPage( navController: NavHostController) {
                     password = it
                 },
                     label = {Text(text = "Password")},
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Password
+                    ),
                     maxLines = 1,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -87,14 +115,23 @@ fun SignUpPage( navController: NavHostController) {
                     confirmPassword = it
                 },
                     label = {Text(text = "Confirm Password")},
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Password
+                    ),
                     maxLines = 1,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
                 )
                 
-                Button(onClick = 
-                { /*TODO*/ 
+                Button(onClick =
+                { /*TODO*/
+                if (password == confirmPassword && password.isNotBlank() && confirmPassword.isNotBlank()) {
+                    navController.navigate(Routes.Login.route)
+                } else {
+                    displayToast(context)
+                }
                 },
                     modifier = Modifier
                         .padding(16.dp)
