92ecb13e575bc510c439020269582fe4e333a30f
TroyFPV48 <77989401+TroyFPV48@users.noreply.github.com>
2023-11-01 18:46:13 -0700

    deleting extra codes

--- app/src/main/java/com/example/nsc_events/data/Event.kt
+++ app/src/main/java/com/example/nsc_events/data/Event.kt
@@ -7,12 +7,7 @@ data class Event(
     val description: String,
     /* TODO: change date variable to a Date data type instead of string */
     val date: String,
-    var eventVisibility : Boolean
     /* TODO: what other event properties should be added? */
 ) {
 
-    fun deleteEvent() {
-        this.eventVisibility = false
-
-    }
 }
