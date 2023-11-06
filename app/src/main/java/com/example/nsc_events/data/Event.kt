44d479a913d5f419d117cd3a3da9ac0f5d099d11
Tuong Pham Minh <77989401+TroyFPV48@users.noreply.github.com>
2023-11-06 15:43:33 -0800

    nsc-event-android-1-52-Event-DTO (#77)
    * actvity formatted
    
    * Retyped event variables, prevented overlapping elements and fixed conflict AGP.

--- app/src/main/java/com/example/nsc_events/data/Event.kt
+++ app/src/main/java/com/example/nsc_events/data/Event.kt
@@ -2,11 +2,36 @@ package com.example.nsc_events.data
 
 import java.util.Date
 
+data class SocialMedia(
+    val facebook: String,
+    val twitter: String,
+    val instagram: String,
+    val hashtag: String
+)
+
 data class Event(
     val eventTitle: String,
     val eventDescription: String,
-    /* TODO: change date variable to a Date data type instead of string */
-    val eventDate: String,
+    val eventCategory: String,
+    val date: Date,
+    val eventStartTime: String,
+    val eventEndTime: String,
+    val eventLocation: String,
+    val eventCoverPhoto: String,
+    val eventHost: String,
+    val eventWebsite: String,
+    val eventRegistration: String,
+    val eventCapacity: String,
+    val eventCost: String,
+    val eventTags: Array<String>,
+    val eventSchedule: String,
+    val eventSpeakers: Array<String>,
+    val eventPrerequisites: String,
+    val eventCancellationPolicy: String,
+    val eventContact: String,
+    val eventSocialMedia: SocialMedia,
+    val eventPrivacy: String,
+    val eventAccessibility: String,
     var eventVisibility : Boolean
 ){
     fun deleteEvent() {
