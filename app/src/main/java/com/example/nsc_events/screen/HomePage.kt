f160b92bbb7cc15a6059a53774946aed0cb280ec
taylorpapke <taylorpapke@gmail.com>
2023-11-07 12:58:09 -0800

    deleted comment to test if CI is triggered on commit and push to PR

--- app/src/main/java/com/example/nsc_events/screen/HomePage.kt
+++ app/src/main/java/com/example/nsc_events/screen/HomePage.kt
@@ -1,6 +1,5 @@
 package com.example.nsc_events.screen
 
-//comment for test pr
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
