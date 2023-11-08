e2e5e550398e989a7ce9492a6b95875ad06f9eee
taylorpapke <taylorpapke@gmail.com>
2023-11-07 23:54:55 -0800

    changed comment slightly to see if it retriggers CI

--- app/src/main/java/com/example/nsc_events/screen/HomePage.kt
+++ app/src/main/java/com/example/nsc_events/screen/HomePage.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.nsc_events.Routes
 
-//comment for testing CI
+//comment for testing CI!
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(navController: NavHostController) { // Create Navbar
