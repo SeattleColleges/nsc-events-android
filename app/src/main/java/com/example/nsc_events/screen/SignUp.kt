3c998d4ac5e70bb2eea4715f85671b2be0515dc3
kkennedy-gh <kylekenne@gmail.com>
2023-11-13 13:31:42 -0800

    Addded scaffold to sign up

--- app/src/main/java/com/example/nsc_events/screen/SignUp.kt
+++ app/src/main/java/com/example/nsc_events/screen/SignUp.kt
@@ -12,11 +12,15 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -34,8 +39,9 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.nsc_events.Routes
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignUpPage( navController: NavHostController) {
+fun SignUpPage(navController: NavHostController) {
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
@@ -45,143 +51,168 @@ fun SignUpPage( navController: NavHostController) {
     var passwordMatchValid by remember { mutableStateOf(true) }
     var matchPasswordSupportingText by remember { mutableStateOf("") }
 
-    Column (
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        IconButton(
-            onClick = { navController.navigate(Routes.HomePage.route) },
-            modifier = Modifier.align(Alignment.Start)
-        ) {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Home")
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Home")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate(Routes.HomePage.route) }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Go back to Homepage"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
         }
-
-        Text ( text = "Sign Up")
-
-        Box(
+    ) { values ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            contentAlignment = Alignment.Center
+                .fillMaxSize()
+                .padding(values)
+                .background(color = MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
+            Text(text = "Sign Up")
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceVariant,
-                        shape = MaterialTheme.shapes.small
-                    )
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
             ) {
-                // TODO: Name Field here
-                TextField(value = name, onValueChange ={
-                    name = it
-                },
-                    label = { Text(text = "Name")},
-                    maxLines = 1,
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
-                    )
-
-                // Email field
-                TextField(value = email, onValueChange ={
-                    email = it
-                    isEmailValid = validateEmail(email)
-                },
-                    isError = !isEmailValid,
-                    label = { Text(text = "Email")},
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Email
-                    ),
-                    maxLines = 1,
-                    supportingText = {
-                                     if(isEmailValid) {
-                                         Text(text = "")
-                                     } else {
-
-                                         Text(text = "Email is not val!", color = MaterialTheme.colorScheme.error)
-                                     }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
-                )
-
-                // Password field
-                TextField(value = password, onValueChange = {
-                    password = it
-                    isPasswordValid = validatePassword(password)
-                },
-                    isError = !isPasswordValid,
-                    label = {Text(text = "Password")},
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Password
-                    ),
-                    maxLines = 1,
-                    supportingText = { if(isPasswordValid){
-                        Text(text = "")
-                    } else {
-                        Text(text = "Password is not valid!", color = MaterialTheme.colorScheme.error)
-                    }},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
-                )
-
-                // confirmPassword field
-                TextField(value = confirmPassword, onValueChange = {
-                    confirmPassword = it
-//                    isPasswordValid = validatePassword(confirmPassword)
-                },
-                    label = {Text(text = "Confirm Password")},
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Password
-                    ),
-                    isError = !passwordMatchValid,
-                    maxLines = 1,
-                    supportingText = { Text(text = matchPasswordSupportingText, color = MaterialTheme.colorScheme.error) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
-                )
-                
-                Button(onClick =
-                {
-                if (password == confirmPassword && password.isNotBlank() && confirmPassword.isNotBlank()) {
-                    navController.navigate(Routes.HomePage.route)
-                } else {
-                    passwordMatchValid = false
-                    matchPasswordSupportingText = "Passwords must match!"
-                }
-                },
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(200.dp)
-                        .align(Alignment.CenterHorizontally),
-                ) {
-                    Text(text = "SIGN UP",
-                        style = TextStyle(
-                            fontSize = 14.sp,
-                            fontFamily = FontFamily.Default,
-                            fontWeight = FontWeight.Bold,
+                        .background(
+                            color = MaterialTheme.colorScheme.surfaceVariant,
+                            shape = MaterialTheme.shapes.small
                         )
+                ) {
+                    // TODO: Name Field here
+                    TextField(
+                        value = name, onValueChange = {
+                            name = it
+                        },
+                        label = { Text(text = "Name") },
+                        maxLines = 1,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
                     )
+
+                    // Email field
+                    TextField(
+                        value = email, onValueChange = {
+                            email = it
+                            isEmailValid = validateEmail(email)
+                        },
+                        isError = !isEmailValid,
+                        label = { Text(text = "Email") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Email
+                        ),
+                        maxLines = 1,
+                        supportingText = {
+                            if (isEmailValid) {
+                                Text(text = "")
+                            } else {
+
+                                Text(
+                                    text = "Email is not val!",
+                                    color = MaterialTheme.colorScheme.error
+                                )
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
+                    )
+
+                    // Password field
+                    TextField(
+                        value = password, onValueChange = {
+                            password = it
+                            isPasswordValid = validatePassword(password)
+                        },
+                        isError = !isPasswordValid,
+                        label = { Text(text = "Password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Password
+                        ),
+                        maxLines = 1,
+                        supportingText = {
+                            if (isPasswordValid) {
+                                Text(text = "")
+                            } else {
+                                Text(
+                                    text = "Password is not valid!",
+                                    color = MaterialTheme.colorScheme.error
+                                )
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
+                    )
+
+                    // confirmPassword field
+                    TextField(
+                        value = confirmPassword, onValueChange = {
+                            confirmPassword = it
+//                    isPasswordValid = validatePassword(confirmPassword)
+                        },
+                        label = { Text(text = "Confirm Password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Password
+                        ),
+                        isError = !passwordMatchValid,
+                        maxLines = 1,
+                        supportingText = {
+                            Text(
+                                text = matchPasswordSupportingText,
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, top = 30.dp, end = 30.dp, bottom = 16.dp)
+                    )
+                    Button(
+                        onClick =
+                        {
+                            if (password == confirmPassword && password.isNotBlank() && confirmPassword.isNotBlank()) {
+                                navController.navigate(Routes.HomePage.route)
+                            } else {
+                                passwordMatchValid = false
+                                matchPasswordSupportingText = "Passwords must match!"
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .width(200.dp)
+                            .align(Alignment.CenterHorizontally),
+                    ) {
+                        Text(
+                            text = "SIGN UP",
+                            style = TextStyle(
+                                fontSize = 14.sp,
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Bold,
+                            )
+                        )
+                    }
                 }
-                
-
             }
-
-
-
-            }
-
-
-
-
         }
     }
+}
