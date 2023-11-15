fa65c149d319b4d4bf5678d89dc73f027c4ffafe
Ali Sharif <alisharif29@gmail.com>
2023-11-14 22:41:33 -0800

    Add back button and fix routes

--- app/src/main/java/com/example/nsc_events/screen/CreatorView.kt
+++ app/src/main/java/com/example/nsc_events/screen/CreatorView.kt
@@ -1,72 +1,113 @@
 package com.example.nsc_events.screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.nsc_events.R
 import com.example.nsc_events.Routes
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CreatorView(navController: NavHostController) {
-    
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { navController.navigate(Routes.AddEvent.route) }) {
-            Text(text = "Create Event",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default,
-                    fontWeight = FontWeight.Bold
-                )
-            )
+    Scaffold (
+        topBar = {
+                 TopAppBar(
+                     title = {
+                         Text(text = "Home")
+                     },
+                     navigationIcon = {
+                         IconButton(
+                             onClick = { navController.navigate(Routes.HomePage.route) }
+                         ) {
+                             Icon(imageVector = Icons.Default.ArrowBack,
+                                 contentDescription = "Go back to Hompage")
+                         }
+                     },
+                     scrollBehavior = scrollBehavior
+
+                     )
+
+        },
+
+
+        content = {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.background),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Button(onClick = { navController.navigate(Routes.AddEvent.route) }) {
+                    Text(text = "Create Event",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(onClick = { navController.navigate(Routes.HomePage.route) }) {
+                    Text(text = "View My Events",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(onClick = { navController.navigate(Routes.HomePage.route) }) {
+                    Text(text = "View All Events",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+
+            }
+
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { navController.navigate(Routes.HomePage.route) }) {
-            Text(text = "View My Events",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { navController.navigate(Routes.AddEvent.route) }) {
-            Text(text = "View All Events",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-        }
-        
-    }
-
-
+    )
 
 }
--- app/src/main/java/com/example/nsc_events/screen/HomePage.kt
+++ app/src/main/java/com/example/nsc_events/screen/HomePage.kt
@@ -74,7 +74,7 @@ fun HomePage(navController: NavHostController) { // Create Navbar
                 navController.navigate(Routes.CreatorView.route)
         }
         ) {
-            Text(text="Sign-up")
+            Text(text="Creator View")
         }
         Spacer(modifier = Modifier.height(16.dp))
         Button(
