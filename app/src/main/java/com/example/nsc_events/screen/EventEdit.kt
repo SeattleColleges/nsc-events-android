771578365efc8a2ea54ce4673c09991bea4646a9
Jeremy Ward <exochos@gmail.com>
2023-11-20 17:53:32 -0800

    Added Routing
    Added routing and a blank page to route into

---
+++ app/src/main/java/com/example/nsc_events/screen/EventEdit.kt
@@ -0,0 +1,9 @@
+package com.example.nsc_events.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun EventEditPage(navController: NavController, eventId: String) {
+    // TODO: Implement the UI and functionality for event editing
+}
