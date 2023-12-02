46a2e4015c02ed20c4d533a2a45833fbe60b8dbf
Ali <alisharif29@gmail.com>
2023-12-02 02:47:13 -0800

    nsc_android_feature_147_adminviewpage (#152)
    * Create input for Sign Up page
    
    * Add Name field & Password match toast
    
    * Add icon button leading to homepage
    
    * Replace toast with supportingText
    
    * Add validation for Email, Password, and confirmPassword
    
    * Fix validation to use isError
    
    * Create creator view page
    
    * Add back button and fix routes
    
    * Create AdminView page
    
    * Add Admin view page & disable edit role button

--- app/src/main/java/com/example/nsc_events/Routes.kt
+++ app/src/main/java/com/example/nsc_events/Routes.kt
@@ -3,12 +3,11 @@ package com.example.nsc_events
 sealed class Routes(val route: String) {
     object Login : Routes("Login")
     object SignUp : Routes("SignUp")
-
     object AddEvent: Routes("Add Event")
     object HomePage: Routes("Home")
     object EventDetail: Routes("Event Detail")
     object EventEdit: Routes("Edit Event")
     object ForgotPassword: Routes("Forgot Password")
-
     object CreatorView: Routes("Creator View")
+    object AdminView: Routes(" Admin View")
 }
